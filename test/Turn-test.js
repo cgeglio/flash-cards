710e980b9b78cb4bf4eb56c88c18d79c799e0a62
Carla Geglio <carla.geglio@gmail.com>
2019-11-18 18:48:29 -0700

    Write and pass first two Turn tests

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -2,15 +2,24 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Turn = require('../src/Turn');
+const Card = require('../src/Card');
 
-
-describe('Card', function() {
+describe('Turn', function() {
 
   let card;
   let turn;
 
   beforeEach(function () {
-    card = new Card(new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');)
+    card = new Card(new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter'));
     turn = new Turn('pug', card);
   });
+
+  it('should be a function', function() {
+    expect(Turn).to.be.a('function');
+  });
+
+  it('should be an instance of Turn', function() {
+    expect(turn).to.be.an.instanceof(Turn);
+  });
+
 });
