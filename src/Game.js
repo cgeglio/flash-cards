5a2427bae1e922ff50593613fe03349897376fb0
Carla Geglio <carla.geglio@gmail.com>
2019-11-19 15:44:28 -0700

    Add start method to Game class and invoke in node.js

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,28 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+const Round = require('../src/Round');
 
 class Game {
-  constructor() {}
+  constructor() {
+    this.currentRound;
+    this.cards = [];
+  }
+
+  start() {
+    for (var i = 0; i < prototypeQuestions.length; i++) {
+      var card = new Card(prototypeQuestions[i]);
+      this.cards.push(card);
+    }
+
+    var deck = new Deck(this.cards);
+    this.currentRound = new Round(deck);
+
+    this.printMessage(deck, this.currentRound);
+    this.printQuestion(this.currentRound);
+  }
 
   printMessage(deck, round) {
       console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
@@ -13,6 +32,8 @@ class Game {
   printQuestion(round) {
       util.main(round);
   }
+
+
 }
 
 module.exports = Game;
